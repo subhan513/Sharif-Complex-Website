63c7bb6934cd5fbb95865da28f49c01338c1c6ad
Najam us Saqib <najamforit@gmail.com>
2023-03-31 07:48:27 +0500

    c

--- components/nav.js
+++ components/nav.js
@@ -412,8 +412,8 @@ export default function Nav() {
                       </button>
                     </div>
                   </Transition.Child>
-                  <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-                    <div className="px-4 sm:px-6 mx-auto">
+                  <div className="h-full flex flex-col py-3 bg-white shadow-xl overflow-y-scroll">
+                    <div className="px-4 sm:px-3 mx-auto">
                       <Dialog.Title className="text-2xl font-bold text-gray-900">
                       <Image
               src="/images/spica-logo.png"
