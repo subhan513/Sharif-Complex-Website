19e3903c4fcc6fb9713371a2fea1a0e0f979319f
Najam us Saqib <najamforit@gmail.com>
2023-04-03 01:09:50 +0500

    c

--- pages/index.js
+++ pages/index.js
@@ -673,15 +673,15 @@ export default function Home() {
           <div className="container mx-auto px-5 sec-cta">
             <div className="logo-badge">
               <Image
-                src="/images/solnyne-logo.png"
+                src="/images/spica-logo2.png"
                 className="w-40 md:w-52 h-auto"
-                alt="Solnyne Solutions"
+                alt="Spica International School"
                 width={208}
                 height={140}
               ></Image>
             </div>
             <h2>
-            Let's discuss 
+            It's a bird! It's a plane! 
               <span className="marksvg">
                 <svg
                   aria-hidden="true"
@@ -691,20 +691,21 @@ export default function Home() {
                 >
                   <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
                 </svg>
-                <span className="relative block md:inline"> the details of your solar project! </span>
+                <span className="relative block md:inline"> It's your three-year-old! </span>
               </span>
             </h2>
 
             <p>
 
-            Our team of experts is ready to answer any questions you may have and help you find the best solution for your needs. Contact us today to schedule a consultation.
+            To your presschooler, nothing is impossible! They are opening up their world and developing
+            special friendship. Together, they are learning new words and forming new ideas every day.
  
             </p>
  
 
             <div className="social">
               <a
-                href="https://wa.me/+923000601194"
+                href="https://wa.me/+9231234567"
                 title="WhatsApp"
                 className="tooltip hover:-translate-y-2 duration-300 transition"
               >
@@ -724,7 +725,7 @@ export default function Home() {
                 </svg>
               </a>
 
-              <a href="tel:+923041111908" title="Call" className="tooltip  hover:-translate-y-2 duration-300 transition">
+              <a href="tel:+92321234567" title="Call" className="tooltip  hover:-translate-y-2 duration-300 transition">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
